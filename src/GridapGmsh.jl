0c0190ff41e6a02f2d2f5459899e434bcb8f56d7
fverdugo <fverdugo@cimne.upc.edu>
2019-07-31 16:20:11 +0200

    Added basic structure

--- src/GridapGmsh.jl
+++ src/GridapGmsh.jl
@@ -1,5 +1,16 @@
 module GridapGmsh
 
-greet() = print("Hello World!")
+using Libdl
+
+include(joinpath(@__DIR__, "..", "deps", "deps.jl"))
+
+include(gmsh_jl)
+
+# Hack taken from https://github.com/JuliaParallel/MPI.jl/blob/master/src/MPI.jl
+function __init__()
+    @static if Sys.isunix()
+        Libdl.dlopen(gmsh.lib, Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
+    end
+end
 
 end # module
