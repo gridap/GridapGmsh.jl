1997cc12e73cadbccccaf2256302be76cc5585d3
fverdugo <fverdugo@cimne.upc.edu>
2019-07-31 16:43:59 +0200

    error if deps.jl not found

--- src/GridapGmsh.jl
+++ src/GridapGmsh.jl
@@ -2,11 +2,20 @@ module GridapGmsh
 
 using Libdl
 
-include(joinpath(@__DIR__, "..", "deps", "deps.jl"))
+deps_jl = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(deps_jl)
+  s = """
+  Package GridapGmsh not installed properly.
+  Run Pkg.build(\"GridapGmsh\"), restart Julia and try again
+  """
+  error(s)
+end
+
+include(deps_jl)
 
 include(gmsh_jl)
 
-# Hack taken from https://github.com/JuliaParallel/MPI.jl/blob/master/src/MPI.jl
+# Hack taken from MPI.jl
 function __init__()
     @static if Sys.isunix()
         Libdl.dlopen(gmsh.lib, Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
